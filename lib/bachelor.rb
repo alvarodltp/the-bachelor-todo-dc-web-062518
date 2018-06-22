cf30918234a97fbe59ee92376673bae41a3e9723
Alvaro De La Torre <alvarodltp@gmail.com>
2018-06-22 15:57:03 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,5 +48,16 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  
+  age = 0 
+  count = 0 
+  data[season].each do |hash|
+    hash.each do |k, v|
+      if k == "age"
+        count += 1 
+        age += v.to_f 
+      end
+    end
+  end
+  avg = (age/count).round 
+  return avg
 end
